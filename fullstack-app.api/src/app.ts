5cd6282135b56469f551524b800002d2215d749e
Victor S. Bitencourt <victor.s.bitencourt@gmail.com>
2024-05-25 21:07:28 -0300

    feat: add api and database containers

--- fullstack-app.api/src/app.ts
+++ fullstack-app.api/src/app.ts
@@ -1,18 +1,17 @@
 import express, { Express, Request, Response } from 'express';
 import * as bodyParser from 'body-parser';
-import dotenv from 'dotenv';
 import routes from './routes';
-import { connection } from './config/db';
 
+import dotenv from 'dotenv';
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.API_PORT || 3000;
+const port = process.env.NODE_PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', async (_req: Request, res: Response) => res.send('Hello, world!'));
+app.get('/', async (_req: Request, res: Response) => res.send('Hello, world!!!'));
 
 app.use('/api', routes);
 
--- fullstack-app.api/src/config/db.ts
+++ fullstack-app.api/src/config/db.ts
@@ -1,9 +1,9 @@
 import { createPool } from 'mysql2/promise';
 
 export const connection = createPool({
-  host: '127.0.0.1',
-  port: 3306,
-  user: 'root',
-  password: 'admin',
-  database: 'test_db',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_LOCAL_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_ROOT_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
