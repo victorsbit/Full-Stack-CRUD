fb565d0744dc08de72f68faf00f4e63a167eff59
Victor S. Bitencourt <victor.s.bitencourt@gmail.com>
2024-05-27 21:36:58 -0300

    chore: add CORS policy

--- fullstack-app.api/src/app.ts
+++ fullstack-app.api/src/app.ts
@@ -1,12 +1,14 @@
 import express, { Express, Request, Response } from 'express';
 import * as bodyParser from 'body-parser';
 import routes from './routes';
+import cors from 'cors';
 
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.NODE_PORT || 3000;
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
