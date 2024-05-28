abb1472605bac440d780c0ab5dcb03b19103a816
Victor S. Bitencourt <victor.s.bitencourt@gmail.com>
2024-05-28 16:07:50 -0300

    build: add automatic seeding

--- fullstack-app.api/src/config/db.ts
+++ fullstack-app.api/src/config/db.ts
@@ -1,4 +1,10 @@
 import { createPool } from 'mysql2/promise';
+import fs from 'fs';
+import path from 'path';
+
+const seedQuery = fs.readFileSync(path.resolve(__dirname, './seed.sql'), {
+  encoding: 'utf-8',
+});
 
 export const connection = createPool({
   host: process.env.DB_HOST,
@@ -6,4 +12,8 @@ export const connection = createPool({
   user: process.env.DB_USER,
   password: process.env.DB_ROOT_PASSWORD,
   database: process.env.DB_DATABASE,
+  multipleStatements: true,
 });
+
+console.log('Running SQL seed...');
+connection.query(seedQuery);
---
+++ fullstack-app.api/src/config/seed.sql
@@ -0,0 +1,10 @@
+USE crud_db;
+
+CREATE TABLE User (
+    ID int NOT NULL AUTO_INCREMENT,
+    FirstName varchar(255) NOT NULL,
+    LastName varchar(255) NOT NULL,
+    Email varchar(255) NOT NULL,
+    Password varchar(255) NOT NULL,
+    PRIMARY KEY (ID)
+);
